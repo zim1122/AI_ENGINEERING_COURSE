8b314d7759ef20ac544e7b142b06687467b6933a
Md.Mohtasim Billah Zim <151844024+zim1122@users.noreply.github.com>
2025-12-10 22:38:15 +0600

    Add list operations and print statements in Day_3.py

--- Day_3.py
+++ Day_3.py
@@ -1,1 +1,25 @@
-
+text="let's learn python"
+print(text)
+text='hello "world"'
+print(text)
+num=25
+print(str(25))
+st="Bangladesh"
+# LIST IN PYTHON
+# ITEM1="bREAD"
+# TEAM2="PASTA"
+# ITEM3="FRUITS"
+ITEMS=["BREAD","PASTA","FRUITS","VEGGIES"]
+print(ITEMS)
+print(ITEMS[0])
+ITEMS[0]='CHIPS'
+print(ITEMS)
+print(ITEMS[0:2])
+ITEMS.append("butter")
+print(ITEMS)
+ITEMS.insert(1,"ZIM")
+print(ITEMS)
+food=["bread","pasta","fruits"]
+bathroom=["shampoo","soap"]
+items=food+bathroom
+print(items)
