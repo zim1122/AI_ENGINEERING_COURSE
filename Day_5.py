93f09556330a80bf31007839f36a3712c75ce6a2
Md.Mohtasim Billah Zim <151844024+zim1122@users.noreply.github.com>
2025-12-17 00:06:49 +0600

    Implement expense calculation and summation functions
    Added functions to calculate and print total expenses for Tom and Joe, and a sum function.

--- Day_5.py
+++ Day_5.py
@@ -1,1 +1,29 @@
+def calculate_total(exp):
+    total=0
+    for item in exp:
+        total+=item
+    return total
+tom_exp_list=[2100,3400,3500]
 
+joe_exp_list=[200,500,700]
+
+toms_total=calculate_total(tom_exp_list)
+joe_total=calculate_total(joe_exp_list)
+
+print("Toms total expesnse",toms_total)
+print("Joes total expense ",joe_total)
+total=0
+for item in tom_exp_list:
+    total+=item
+print("Toms total expense:",total)
+
+total=0
+for item in joe_exp_list:
+    total+=item
+print("Joes total expense",total)
+def sum(a,b):
+    total=a+b
+    return total
+
+n=sum(5,6)
+print("Total",n)
